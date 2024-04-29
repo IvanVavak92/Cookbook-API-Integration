c853caf0ec8e7cefbd6145aa75bb2500bbde1b45
Ivan Vavak <ivanvavak92@gmail.com>
2024-04-29 10:28:04 +0200

    added controller with login enpoint and disable spring sec

--- cookbook/src/main/java/com/ivan/cookbook/CookbookApplication.java
+++ cookbook/src/main/java/com/ivan/cookbook/CookbookApplication.java
@@ -2,8 +2,9 @@ package com.ivan.cookbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CookbookApplication {
 
     public static void main(String[] args) {
