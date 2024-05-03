c738ed25bbebc5ed8866b57d85b1416321cafd07
Ivan Vavak <ivanvavak92@gmail.com>
2024-05-03 15:41:48 +0200

    added web config

---
+++ cookbook/src/main/java/com/ivan/cookbook/config/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.ivan.cookbook.config;
+
+import com.ivan.cookbook.filters.JwtAuthFilter;
+import com.ivan.cookbook.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authenticationProvider(authenticationProvider())
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/home").authenticated()
+                                .anyRequest().permitAll()
+                )
+                .formLogin(formLogin ->
+                        formLogin.loginPage("/login") // Use custom login page
+                                .usernameParameter("email")
+                                .defaultSuccessUrl("/home")
+                                .permitAll()
+                )
+                .logout(logout -> logout.logoutSuccessUrl("/").permitAll())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
+
