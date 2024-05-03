ea8c40fb8a5e9f4db4801b4770a6347c1256d99d
Ivan Vavak <ivanvavak92@gmail.com>
2024-05-03 15:10:26 +0200

    added customerUSerDetails and CustomUserDetailsService

---
+++ cookbook/src/main/java/com/ivan/cookbook/services/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.ivan.cookbook.services;
+
+import com.ivan.cookbook.config.CustomerUserDetails;
+import com.ivan.cookbook.models.User;
+import com.ivan.cookbook.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found.");
+        }
+
+        return new CustomerUserDetails(user);
+    }
+}
